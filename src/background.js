b6e0740ff3f6801b822b7ade2d64e0d710b8af7f
sofakeer <sofakeer@gmail.com>
2022-06-13 21:33:01 +0800

    update url

--- src/background.js
+++ src/background.js
@@ -2,6 +2,6 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
   // Send a message to the active tab
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.create({ url: "https://www.baidu.com" });
+    chrome.tabs.create({ url: "https://github.com/sofakeer/justgo" });
   });
 });
